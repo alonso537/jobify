d4ff436d4ebbd06074946f43e9a0c9e33d21194a
alonso537 <58403576+alonso537@users.noreply.github.com>
2022-06-18 17:18:12 -0500

    protected routes and layout routes and outled

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import { Dashboard, Error, Landing, Register } from "./pages";
+import { Error, Landing, ProtectedRoute, Register } from "./pages";
+import {
+  AddJob,
+  AllJobs,
+  Profile,
+  SharedLayout,
+  Stats,
+} from "./pages/dashboard";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Dashboard />} />
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <SharedLayout />
+            </ProtectedRoute>
+          }
+        >
+          <Route index element={<Stats />} />
+          <Route path="all-jobs" element={<AllJobs />} />
+          <Route path="add-job" element={<AddJob />} />
+          <Route path="profile" element={<Profile />} />
+        </Route>
         <Route path="/register" element={<Register />} />
         <Route path="/landing" element={<Landing />} />
         <Route path="*" element={<Error />} />
